0b3aad0d251ffd94f1cce5a9f66f518ea5749678
Kim JH Desktop <kim_jh6859@naver.com>
2023-10-06 22:59:33 +0900

    캐릭터 이동, 카메라 이동, 마우스 회전

---
+++ Assets/scripts/MyUnit.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyUnit : MonoBehaviour
+{
+    public float moveSpeed = 10.0f; // 이동 속도
+    public float rotationSpeed = 2.0f; // 회전 속도
+    private CharacterController character; // 캐릭터 컨트롤러
+    private Transform cameraTransform; // 카메라의 Transform
+
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+        cameraTransform.parent = transform; // 카메라를 물체의 자식으로 설정
+        cameraTransform.localPosition = new Vector3(0, 2, -5); // 카메라의 로컬 위치 조절
+    }
+
+    void Update()
+    {
+        Move();
+        RotateWithMouse();
+    }
+
+    void RotateWithMouse()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // 물체 회전
+        Vector3 rotation = new Vector3(0, mouseX * rotationSpeed, 0);
+        transform.Rotate(rotation);
+
+        // 카메라 회전
+        cameraTransform.Rotate(-mouseY * rotationSpeed, 0, 0);
+
+        // 물체와 카메라가 항상 같은 방향을 보도록 설정
+        cameraTransform.LookAt(transform);
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.TransformDirection(new Vector3(moveX, 0, moveZ));
+        character.SimpleMove(moveDirection * moveSpeed);
+    }
+}
