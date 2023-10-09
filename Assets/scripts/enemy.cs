2f4f9647436238bfee9e12329ab61a514a609113
yong-won-kim <qsc753949@gmail.com>
2023-10-09 23:07:42 +0930

    feat: make enemy

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    Material mat;
+    void Awake(){
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponent<MeshRenderer>().material;
+        curHealth = maxHealth;
+    }
+
+    void OnTriggerEnter(Collider other){
+        // if(other.tag == "Melee"){
+        //     Weapon weapon = other.GetComponent<Weapon>();
+        //     //플레이어 스텟으로 인한 공격력이 weapon.damage 올리는 방식으로 하자
+        //     curHealth -= weapon.damage;
+        //     Vector3 reactVec = transform.position - other.transform.position;
+        //     StartCoroutine(OnDamage());
+        // }
+        // else if(other.tag == "magic"){
+        //     Magic magic = other.GetComponent<Magic>();
+        //     //플레이어 스텟으로 인한 공격력이 weapon.damage 올리는 방식으로 하자
+        //     curHealth -= magic.damage;
+        //     Vector3 reactVec = transform.position - other.transform.position;
+        //     StartCoroutine(OnDamage());
+        // }
+    }
+    // Start is called before the first frame update
+    IEnumerator OnDamage(Vector3 reactVec){
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if(curHealth<0){
+            mat.color = Color.white;
+        }
+        else{
+            mat.color = Color.gray;
+            gameObject.layer = 7;
+            Destroy(gameObject,4);
+            reactVec = reactVec.normalized;
+            reactVec +=Vector3.up;
+            rigid.AddForce(reactVec *5,ForceMode.Impulse);
+        }
+    }
+}
