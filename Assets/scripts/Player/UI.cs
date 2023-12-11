806e0090800681499bf6e4a603399fe296dec071
Kim JH Desktop <kim_jh6859@naver.com>
2023-12-11 20:29:15 +0900

    UI 수정

--- Assets/scripts/Player/UI.cs
+++ Assets/scripts/Player/UI.cs
@@ -10,16 +10,16 @@ public class UI : MonoBehaviour
     [SerializeField]
     public Player player;
 
-    public Slider hpbar;
+    public Image hpbar;
     public TextMeshProUGUI hp;
-    public Slider mpbar;
+    public Image mpbar;
     public TextMeshProUGUI mp;
-    public Slider expbar;
+    public Image expbar;
     public TextMeshProUGUI exp;
 
     void Awake()
     {
-         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
         // players 배열이 비어있지 않은 경우, 첫 번째 플레이어를 선택
         if (players.Length > 0)
@@ -31,6 +31,9 @@ public class UI : MonoBehaviour
             // players 배열이 비어있는 경우, 원하는 처리를 수행하거나 예외 처리를 추가할 수 있습니다.
             Debug.LogError("Player not found in the scene.");
         }
+        hpbar = transform.Find("FrontHealthBar").GetComponent<Image>();
+        mpbar = transform.Find("FrontManaBar").GetComponent<Image>();
+        hpbar = transform.Find("FrontXpBar").GetComponent<Image>();
     }
 
     void Update()
@@ -42,19 +45,19 @@ public class UI : MonoBehaviour
 
     private void HandleHpBar()
     {
-        hpbar.value = (float)player.health / (float)player.maxHealth;
+        hpbar.fillAmount = (float)player.health / (float)player.maxHealth;
         hp.text = player.health.ToString();
     }
 
     private void HandleMpBar()
     {
-        mpbar.value = (float)player.mana / (float)player.maxMana;
+        mpbar.fillAmount = (float)player.mana / (float)player.maxMana;
         mp.text = player.mana.ToString();
     }
 
     private void HandleExpBar()
     {
-        expbar.value = (float)player.exp / (float)(100f + player.level * 10);
+        expbar.fillAmount = (float)player.exp / (float)(100f + player.level * 10);
         exp.text = player.level.ToString() + " Level";
     }
 }
